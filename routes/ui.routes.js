e60641bca401da17bb0abdfad6d26d6d4d98b332
Prasuna <dahalprasuna2001@gmai.com>
2023-09-07 15:40:06 +0545

    mail routes added

--- routes/ui.routes.js
+++ routes/ui.routes.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 
 const qrRouting = require("../modules/qr/qr.routes.ui");
+const mailRouting = require("../utilitis/mail.service");
 
 router.use("/qr", qrRouting);
-
+router.use("/mail", mailRouting);
 module.exports = router;
