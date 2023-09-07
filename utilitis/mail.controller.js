a4357de14d374785219f383d0c3db8652a348ba7
Prasuna <dahalprasuna2001@gmai.com>
2023-09-07 20:28:14 +0545

    mail update

--- utilitis/mail.controller.js
+++ utilitis/mail.controller.js
@@ -1,18 +1,21 @@
-const nodemailer = require("nodemailer");
+const transporter = require("./mail.service");
 class Mail {
   async sendMail(body) {
-    const mailOptions = {
-      from: "your-email@gmail.com",
-      to: body.email,
-      subject: "QR Code",
-      text: "Here is your QR code.",
-      attachments: [
-        {
-          filename: "qr-code.png",
-          content: body.qrCode,
-        },
-      ],
-    };
+    const qrCode = body.qrCode.replace("data:image/png;base64,", ""),
+      MailOptions = {
+        from: "your-email@gmail.com",
+        to: body.email,
+        subject: "QR Code",
+        text: "Here is your QR code.",
+        attachments: [
+          {
+            filename: "qr-code.png",
+            content: Buffer.from(qrCode, "base64"),
+            cid: "Image_ID",
+            contentDisposition: "inline",
+          },
+        ],
+      };
     transporter.sendMail(mailOptions);
   }
 }
--- utilitis/mail.routes.api.js
+++ utilitis/mail.routes.api.js
@@ -3,15 +3,15 @@ const MailController = require("./mail.controller");
 
 router.post("/", async (req, res, next) => {
   try {
-    const email = await MailController.req.body;
+    const email = await req.body;
     const info = await MailController.sendMail(email);
     // const info = await transporter.sendMail(mailOptions);
-    console.log("Email sent:", info.response);
+    console.log("Email sent:", info.messageId);
 
-    res.status(200).send("QR code sent successfully.");
+    res.status(200).json("QR code sent successfully.");
   } catch (error) {
     console.error("Error:", error);
-    res.status(500).send("Error sending QR code.");
+    res.status(500).json("Error sending QR code.");
   }
 });
 
